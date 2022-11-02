22fc16532c8bc38ae994a01f109d8ca9bd4e71f8
Ryo Somatani <ryo_somatani@outlook.com>
2022-11-02 14:15:28 -0700

    lab-2: commit initial files

---
+++ lab-2/app.js
@@ -0,0 +1,28 @@
+const socialIcons = [
+  {
+    icon: 'instagram',
+    title: 'Instagram',
+  },
+  {
+    icon: 'facebook-f',
+    title: 'Facbook',
+  },
+  {
+    icon: 'twitter',
+    title: 'Twitter',
+  },
+  {
+    icon: 'linkedin',
+    title: 'LinkedIn',
+  },
+  {
+    icon: 'youtube',
+    title: 'YouTube',
+  },
+  {
+    icon: 'github',
+    title: 'Github',
+  },
+];
+
+ReactDOM.render('test', document.getElementById('root'));
