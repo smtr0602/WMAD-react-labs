272e7aaca23997520932d8c304598f1c9807ae15
Ryo Somatani <ryo_somatani@outlook.com>
2022-11-18 15:29:21 -0800

    lab-6: fetch multiple posts and store using useReducer

--- Lab6/src/App.jsx
+++ Lab6/src/App.jsx
@@ -1,29 +1,65 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useEffect, useReducer } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [data, setData] = useState();
-  const [error, setError] = useState();
-
-  const getPostsData = () => {
-    console.log("Fetching...")
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts?_limit=10")
-      .then((data) => setData(data.data))
-      .catch((error) => setError(error.message));
+  const getPostsData = async () => {
+    console.log('Fetching...');
+    try {
+      const res = await axios({
+        method: 'get',
+        url: 'https://jsonplaceholder.typicode.com/posts',
+        params: { _limit: 10 },
+      });
+      dispatch({ type: 'SET_POSTS', payload: res.data });
+    } catch (error) {
+      dispatch({ type: 'SET_ERROR', payload: error });
+    }
   };
 
-  if(!data){
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'SET_POSTS':
+        return {
+          ...state,
+          posts: action.payload,
+        };
+      case 'SET_ERROR':
+        return {
+          ...state,
+          error: action.payload,
+        };
+      default:
+        throw new Error('Wrong type');
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, {
+    posts: [],
+  });
+
+  useEffect(() => {
     getPostsData();
-  }
+  }, []);
 
   return (
     <div className="App">
       <h1>React Lab 6</h1>
-      <p className="msg">
-        {data ? JSON.stringify(data[0]) : error ? error : "loading"}
-      </p>
-      <button className="btn" onClick={() => getPostsData()}>Refetch</button>
+      {state.posts.length ? (
+        <ul className="msg">
+          {state.posts.map((post) => (
+            <li key={post.id} className="msgItem">
+              {post.title}
+            </li>
+          ))}
+        </ul>
+      ) : state.error ? (
+        <p className="msg">{state.error}</p>
+      ) : (
+        <p>loading</p>
+      )}
+      <button className="btn" onClick={() => getPostsData()}>
+        Refetch
+      </button>
     </div>
   );
 }
